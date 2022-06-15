a1ef2184451f53177912124d7daf37ef042b1295
SchegolevOL <92328776+SchegolevOL@users.noreply.github.com>
2022-06-15 20:33:00 +0500

    add driver info

---
+++ Console/Console.App/Consol.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyConsole.App
+{
+    public static class Consol
+    {
+        public static void MyDriveInfo()
+        {
+            DriveInfo[] allDrives = DriveInfo.GetDrives();
+
+            foreach (DriveInfo d in allDrives)
+            {
+                Console.WriteLine(
+                    $"Drive {d.Name}");
+                Console.WriteLine(
+                    $"  Drive type: {d.DriveType}");
+                if (d.IsReady == true)
+                {
+                    Console.WriteLine(
+                        $"  Volume label: {d.VolumeLabel}");
+                    Console.WriteLine(
+                        $"  File system: {d.DriveFormat}");
+                    Console.WriteLine(
+                        $"  Available space to current user:{d.AvailableFreeSpace, 15} bytes");
+                    Console.WriteLine(
+                        $"  Total available space:          {d.TotalFreeSpace, 15} bytes");
+                    Console.WriteLine(
+                        $"  Total size of drive:            {d.TotalSize, 15} bytes ");
+                }
+            }
+        }
+    }
+}
--- Console/Console.App/Program.cs
+++ Console/Console.App/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace MyConsole.App
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string str;
+
+            do
+            {
+                str = Console.ReadLine();
+                string[] consol = str.Split('-');
+                switch (consol[0])
+                {
+                    case ("driver info"):
+                        Consol.MyDriveInfo();
+                        break;
+                    default:
+                        break;
+                }
+
+
+            } while (!str.Equals("exit"));
+        }
+    }
+}
